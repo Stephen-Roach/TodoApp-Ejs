4bdffe0e599a00f23aa6d7cab24323c5c9cc3816
Stephen-Roach <stephenroach21@gmail.com>
2021-03-18 10:10:10 -0500

    added css styling

--- app.js
+++ app.js
@@ -2,37 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
+let items = [];
+
 app.get('/', function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = '';
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-  }
-  res.render('list', { kindOfDay: day });
+  let day = today.toLocaleDateString('en-US', options);
+
+  res.render('list', { kindOfDay: day, newListItem: items });
+});
+
+app.post('/', function (req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
